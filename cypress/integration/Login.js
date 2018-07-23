b33c612817f9e2e5ba89af1c307d52df0d4788d1
Fawfau <fawaz.fauzan@cartenz.co.id>
2018-07-23 13:32:07 +0700

    komit kedua

--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -1,4 +1,4 @@
-describe('Coba Saya', function () {
+describe('Login Page', function () {
   it('Login', function () {
   	cy.visit('http://otmproduct.cartenzgroup.com')
   	cy.get('#login_button').should('be.visible')
@@ -10,8 +10,31 @@ describe('Coba Saya', function () {
   })
 })
 
-describe('Coba akses ke Manajemen Dasbor',function () {
-	it('Akses Dasbos', function () {
+describe('Landing Page',function () {
+	it('Akses ke Landing Page', function () {
 		cy.get('#\\31 _menu > span > div > h4').contains('Manajemen Dasbor').click()
+		cy.get('.title-menu').should('have.text','Dasbor')
+	})
+	it.skip('Next Slide', function () {
+		cy.get('.glyphicon.glyphicon-chevron-right').click()
+		cy.get('.header').contains('Status Objek Pajak')
+		cy.get('.glyphicon.glyphicon-chevron-right').click()
+		cy.get('.header').contains('Transaksi Usaha')
+	})
+	it.skip('Prev Slide', function () {
+		cy.wait(500)
+		cy.get('.glyphicon.glyphicon-chevron-left').click()
+		cy.get('.header').contains('Transaksi Usaha')
+		cy.get('.glyphicon.glyphicon-chevron-left').click()
+		cy.get('.header').contains('Status Objek Pajak')
+	})
+	it('Akses ke OTM Page', function () {
+		cy.reload()
+		cy.get('#userid_field').type('flocs').should('have.value','flocs')
+  		cy.get('#password_field').type('coflocs!')
+  		cy.get('#login_button').click()
+		cy.get('#\\31 _menu > span > div > h4').contains('Manajemen Dasbor').click()
+		cy.get('.title-menu').should('have.text','Dasbor')
+		cy.get('.btn.btn-primary.pull-right').eq(0).click()
 	})
 })
